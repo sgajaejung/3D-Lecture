7512924fdc6040698a23c3c5bc1809aaf1822611
jjuidong <jjuiddong@hanmail.net>
2014-05-13 14:34:32 +0900

    complete gouraud shading project

---
+++ GouraudShading/GouraudShading/GouraudShading.cpp
@@ -0,0 +1,517 @@
+// GouraudShading.cpp : 응용 프로그램에 대한 진입점을 정의합니다.
+//
+
+#include "stdafx.h"
+#include "GouraudShading.h"
+#include "math/Math.h"
+#include "DrawTriangle.h"
+#include <vector>
+#include <fstream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+#define MAX_LOADSTRING 100
+
+// 전역 변수:
+HINSTANCE hInst;								// 현재 인스턴스입니다.
+TCHAR szTitle[MAX_LOADSTRING];					// 제목 표시줄 텍스트입니다.
+TCHAR szWindowClass[MAX_LOADSTRING];			// 기본 창 클래스 이름입니다.
+HWND g_hWnd;
+vector<Vector3> g_vertices;
+vector<Vector3> g_normals;
+vector<int> g_indices;
+Matrix44 g_matWorld;
+Matrix44 g_matLocal;
+Matrix44 g_matView;
+Matrix44 g_matProjection;
+Matrix44 g_matViewPort;
+Vector3 g_cameraPos(0,1000,-1000);
+Vector3 g_cameraLookat(0,0,0);
+
+
+// 이 코드 모듈에 들어 있는 함수의 정방향 선언입니다.
+ATOM				MyRegisterClass(HINSTANCE hInstance);
+BOOL				InitInstance(HINSTANCE, int);
+LRESULT CALLBACK	WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK	About(HWND, UINT, WPARAM, LPARAM);
+void					MainLoop(int elapse_time);
+void					Render(HWND hWnd);
+void					Paint(HWND hWnd, HDC hdc);
+void					ComputeNormals(const vector<Vector3> &vertices, const vector<int> &indices, vector<Vector3> &normals);
+bool					ReadModelFile( const string &fileName );
+
+int APIENTRY _tWinMain(HINSTANCE hInstance,
+                     HINSTANCE hPrevInstance,
+                     LPTSTR    lpCmdLine,
+                     int       nCmdShow)
+{
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+
+ 	// TODO: 여기에 코드를 입력합니다.
+	MSG msg;
+	HACCEL hAccelTable;
+
+	// 전역 문자열을 초기화합니다.
+	LoadString(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+	LoadString(hInstance, IDC_GOURAUDSHADING, szWindowClass, MAX_LOADSTRING);
+	MyRegisterClass(hInstance);
+
+	ReadModelFile("vase.dat");
+
+	g_matWorld.SetTranslate(Vector3(0,0,0));
+
+		// 응용 프로그램 초기화를 수행합니다.
+	if (!InitInstance (hInstance, nCmdShow))
+	{
+		return FALSE;
+	}
+
+	Vector3 dir = g_cameraLookat - g_cameraPos;
+	dir.Normalize();
+	g_matView.SetView(g_cameraPos, dir, Vector3(0,1,0));
+	g_matProjection.SetProjection( MATH_PI / 4.f, 1.0f, 1.0f, 100.0f );
+
+	RECT cr;
+	::GetClientRect(g_hWnd, &cr);
+	const float width = 800;//(float)(cr.right-cr.left);
+	const float height = 600;//(float)(cr.bottom - cr.top);
+	g_matViewPort._11 = width/2;
+	g_matViewPort._22 = -height/2;
+	g_matViewPort._33 = 0;
+	g_matViewPort._41 = width/2;
+	g_matViewPort._42 = height/2;
+	g_matViewPort._43 = 0;
+
+
+	hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_GOURAUDSHADING));
+
+	// 기본 메시지 루프입니다.
+	int oldT = GetTickCount();
+	while (1)
+	{
+		if (PeekMessageA(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+			{
+				TranslateMessage(&msg);
+				DispatchMessage(&msg);
+			}
+			if(msg.message == WM_QUIT)
+				break;
+		}
+
+		const int curT = GetTickCount();
+		const int elapseT = curT - oldT;
+		if (elapseT > 30)
+		{
+			oldT = curT;
+			MainLoop(elapseT);	
+		}
+	}
+
+	return (int) msg.wParam;
+}
+
+
+
+//
+//  함수: MyRegisterClass()
+//
+//  목적: 창 클래스를 등록합니다.
+//
+//  설명:
+//
+//    Windows 95에서 추가된 'RegisterClassEx' 함수보다 먼저
+//    해당 코드가 Win32 시스템과 호환되도록
+//    하려는 경우에만 이 함수를 사용합니다. 이 함수를 호출해야
+//    해당 응용 프로그램에 연결된
+//    '올바른 형식의' 작은 아이콘을 가져올 수 있습니다.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+	WNDCLASSEX wcex;
+
+	wcex.cbSize = sizeof(WNDCLASSEX);
+
+	wcex.style			= CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc	= WndProc;
+	wcex.cbClsExtra		= 0;
+	wcex.cbWndExtra		= 0;
+	wcex.hInstance		= hInstance;
+	wcex.hIcon			= LoadIcon(hInstance, MAKEINTRESOURCE(IDI_GOURAUDSHADING));
+	wcex.hCursor		= LoadCursor(NULL, IDC_ARROW);
+	wcex.hbrBackground	= (HBRUSH)(COLOR_WINDOW+1);
+	wcex.lpszMenuName	= MAKEINTRESOURCE(IDC_GOURAUDSHADING);
+	wcex.lpszClassName	= szWindowClass;
+	wcex.hIconSm		= LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+	return RegisterClassEx(&wcex);
+}
+
+//
+//   함수: InitInstance(HINSTANCE, int)
+//
+//   목적: 인스턴스 핸들을 저장하고 주 창을 만듭니다.
+//
+//   설명:
+//
+//        이 함수를 통해 인스턴스 핸들을 전역 변수에 저장하고
+//        주 프로그램 창을 만든 다음 표시합니다.
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   HWND hWnd;
+
+   hInst = hInstance; // 인스턴스 핸들을 전역 변수에 저장합니다.
+
+   g_hWnd = hWnd = CreateWindow(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+      0, 0, 800, 600, NULL, NULL, hInstance, NULL);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+
+   return TRUE;
+}
+
+//
+//  함수: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  목적: 주 창의 메시지를 처리합니다.
+//
+//  WM_COMMAND	- 응용 프로그램 메뉴를 처리합니다.
+//  WM_PAINT	- 주 창을 그립니다.
+//  WM_DESTROY	- 종료 메시지를 게시하고 반환합니다.
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	int wmId, wmEvent;
+	PAINTSTRUCT ps;
+	HDC hdc;
+
+	switch (message)
+	{
+	case WM_COMMAND:
+		wmId    = LOWORD(wParam);
+		wmEvent = HIWORD(wParam);
+		// 메뉴 선택을 구문 분석합니다.
+		switch (wmId)
+		{
+		case IDM_ABOUT:
+			DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+			break;
+		case IDM_EXIT:
+			DestroyWindow(hWnd);
+			break;
+		default:
+			return DefWindowProc(hWnd, message, wParam, lParam);
+		}
+		break;
+	case WM_ERASEBKGND:
+		return 0;
+	case WM_PAINT:
+		hdc = BeginPaint(hWnd, &ps);
+		Paint(hWnd, hdc);
+		EndPaint(hWnd, &ps);
+		break;
+	case WM_KEYDOWN:
+		{
+			switch (wParam)
+			{
+			case 'E':
+			case 'C':
+				{
+					Matrix44 mat;
+					mat.SetRotationY( (wParam == 'E')? 0.1f : -0.1f);
+					g_cameraPos *= mat;
+					Vector3 dir = g_cameraLookat - g_cameraPos;
+					dir.Normalize();
+					g_matView.SetView(g_cameraPos, dir, Vector3(0,1,0));
+				}
+				break;
+
+			case VK_UP:
+			case VK_DOWN:
+				{
+					Matrix44 mat;
+					mat.SetRotationX((wParam==VK_UP)? 0.1f : -0.1f);
+					g_matLocal *= mat;
+				}
+				break;
+
+			case VK_LEFT:
+			case VK_RIGHT:
+				{
+					Matrix44 mat;
+					mat.SetRotationY((wParam==VK_LEFT)? 0.1f : -0.1f);
+					g_matLocal *= mat;
+				}
+				break;
+			}
+
+		}
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
+}
+
+// 정보 대화 상자의 메시지 처리기입니다.
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	UNREFERENCED_PARAMETER(lParam);
+	switch (message)
+	{
+	case WM_INITDIALOG:
+		return (INT_PTR)TRUE;
+
+	case WM_COMMAND:
+		if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+		{
+			EndDialog(hDlg, LOWORD(wParam));
+			return (INT_PTR)TRUE;
+		}
+		break;
+	}
+	return (INT_PTR)FALSE;
+}
+
+
+
+/**
+ @brief 
+ */
+void	MainLoop(int elapse_time)
+{
+	// Render
+	Render(g_hWnd);
+	::InvalidateRect(g_hWnd, NULL, TRUE);
+}
+
+
+/**
+ @brief 
+ */
+void	Render(HWND hWnd)
+{
+	HDC hdc = GetDC(hWnd);
+	Paint(hWnd, hdc);
+	::ReleaseDC(hWnd, hdc);
+}
+
+
+/**
+ @brief 
+ @date 2014-04-07
+*/
+void ComputeNormals(const vector<Vector3> &vertices, const vector<int> &indices, vector<Vector3> &normals)
+{
+	g_normals.resize(vertices.size());
+
+	for (unsigned int i=0; i < indices.size(); i+=3)
+	{
+		Vector3 p1 = vertices[ indices[ i]];
+		Vector3 p2 = vertices[ indices[ i+1]];
+		Vector3 p3 = vertices[ indices[ i+2]];
+
+		Vector3 v1 = p2 - p1;
+		Vector3 v2 = p3 - p1;
+		v1.Normalize();
+		v2.Normalize();
+		Vector3 n = v1.CrossProduct(v2);
+		n.Normalize();
+
+		if (g_normals[ indices[ i]].IsEmpty())
+		{
+			g_normals[ indices[ i]] = n;
+		}
+		else
+		{
+			g_normals[ indices[ i]] += n;
+			g_normals[ indices[ i]] /= 2.f;
+		}
+
+		if (g_normals[ indices[ i+1]].IsEmpty())
+		{
+			g_normals[ indices[ i+1]] = n;
+		}
+		else
+		{
+			g_normals[ indices[ i+1]] += n;
+			g_normals[ indices[ i+1]] /= 2.f;
+		}
+
+		if (g_normals[ indices[ i+2]].IsEmpty())
+		{
+			g_normals[ indices[ i+2]] = n;
+		}
+		else
+		{
+			g_normals[ indices[ i+2]] += n;
+			g_normals[ indices[ i+2]] /= 2.f;
+		}
+	}
+}
+
+
+bool ReadModelFile( const string &fileName )
+{
+	using namespace std;
+	ifstream fin(fileName.c_str());
+	if (!fin.is_open())
+		return false;
+
+	string vtx, vtx_eq;
+	int numVertices;
+	fin >> vtx >> vtx_eq >> numVertices;
+
+	if (numVertices <= 0)
+		return  false;
+
+	g_vertices.resize(numVertices);
+
+	float num1, num2, num3;
+	for (int i = 0; i < numVertices; i++)
+	{
+		fin >> num1 >> num2 >> num3;
+		g_vertices[i] = Vector3(num1, num2, num3);
+	}
+
+	string idx, idx_eq;
+	int numIndices;
+	fin >> idx >> idx_eq >> numIndices;
+
+	if (numIndices <= 0)
+		return false;
+
+	g_indices.resize(numIndices*3);
+
+	int num4, num5, num6;
+	for (int i = 0; i < numIndices*3; i+=3)
+	{
+		fin >> num4 >> num5 >> num6;
+		g_indices[ i] = num4;
+		g_indices[ i+1] = num5;
+		g_indices[ i+2] = num6;	
+	}
+
+	ComputeNormals(g_vertices, g_indices, g_normals);
+
+	return true;
+}
+
+
+void RenderVertices(HDC hdc, const vector<Vector3> &vertices, const Matrix44 &tm)
+{
+	for (unsigned int i=0; i < vertices.size(); ++i)
+	{
+		Vector3 p = vertices[ i] * tm;
+
+		if (0 == i)
+			MoveToEx(hdc, (int)p.x, (int)p.y, NULL);
+		else
+			LineTo(hdc, (int)p.x, (int)p.y);
+	}
+}
+
+
+void RenderIndices(HDC hdc, const vector<Vector3> &vertices, const vector<int> &indices, 
+				   const vector<Vector3> &normals, const Matrix44 &tm, const Matrix44 &vpv)
+{
+	Vector3 camDir = g_cameraLookat - g_cameraPos;
+	camDir.Normalize();
+
+	for (unsigned int i=0; i < indices.size(); i+=3)
+	{
+		Vector3 p1 = vertices[ indices[ i]];
+		Vector3 p2 = vertices[ indices[ i+1]];
+		Vector3 p3 = vertices[ indices[ i+2]];
+
+		p1 = p1 * tm;
+		p2 = p2 * tm;
+		p3 = p3 * tm;
+
+		// culling
+		Vector3 n1 = normals[ indices[i]] * tm;
+		Vector3 n2 = normals[ indices[i+1]] * tm;
+		Vector3 n3 = normals[ indices[i+2]] * tm;
+		const float dot1 = n1.DotProduct(camDir);
+		const float dot2 = n2.DotProduct(camDir);
+		const float dot3 = n3.DotProduct(camDir);
+		if ((dot1 > 0) && (dot2 > 0) && (dot3 > 0))
+			continue;
+
+		p1 = p1 * vpv;
+		p2 = p2 * vpv;
+		p3 = p3 * vpv;
+
+		Rasterizer::Color c0(0,0,255,1);
+		Rasterizer::Color c1(255,255,255,1);
+		Vector3 lightDir(0,-1,0);
+		Vector3 H = -(camDir + lightDir);
+		H.Normalize();
+		Rasterizer::Color ambient(0,20,0,1);
+
+		Rasterizer::Color color1, color2, color3;
+		{
+			Rasterizer::Color diffuse = c0 * max(0, n1.DotProduct(-lightDir));
+			Rasterizer::Color specular = c1*pow(n1.DotProduct(H), 16);
+			color1 = ambient + diffuse + specular;
+		}
+		{
+			Rasterizer::Color diffuse = c0 * max(0, n2.DotProduct(-lightDir));
+			Rasterizer::Color specular = c1*pow(n2.DotProduct(H), 16);
+			color2 = ambient + diffuse + specular;
+		}
+		{
+			Rasterizer::Color diffuse = c0 * max(0, n3.DotProduct(-lightDir));
+			Rasterizer::Color specular = c1*pow(n3.DotProduct(H), 16);
+			color3 = ambient + diffuse + specular;
+		}
+
+//		Rasterizer::DrawLine(hdc, color, p1.x, p1.y, color, p2.x, p2.y);
+//		Rasterizer::DrawLine(hdc, color, p1.x, p1.y, color, p3.x, p3.y);
+//		Rasterizer::DrawLine(hdc, color, p3.x, p3.y, color, p2.x, p2.y);
+		Rasterizer::DrawTriangle(hdc, 
+			color1, p1.x, p1.y, n1,
+			color2, p2.x, p2.y, n2,
+			color3, p3.x, p3.y, n3);
+	}
+}
+
+
+/**
+ @brief 
+ */
+void Paint(HWND hWnd, HDC hdc)
+{
+	RECT rc;
+	GetClientRect(hWnd, &rc);
+	HDC hdcMem = CreateCompatibleDC(hdc);
+	HBITMAP hbmMem = CreateCompatibleBitmap(hdc, rc.right-rc.left, rc.bottom-rc.top);
+	HBITMAP hbmOld = (HBITMAP)SelectObject(hdcMem, hbmMem);
+	HBRUSH hbrBkGnd = CreateSolidBrush(GetSysColor(COLOR_WINDOW));
+	FillRect(hdcMem, &rc, hbrBkGnd);
+	DeleteObject(hbrBkGnd);
+
+	Matrix44 vpv = g_matView * g_matProjection * g_matViewPort;
+	RenderIndices(hdcMem, g_vertices, g_indices, g_normals, g_matLocal * g_matWorld, vpv);
+
+	BitBlt(hdc, rc.left, rc.top, rc.right-rc.left, rc.bottom-rc.top, hdcMem, 0, 0, SRCCOPY);
+	SelectObject(hdcMem, hbmOld);
+	DeleteObject(hbmMem);
+	DeleteDC(hdcMem);
+}
+
