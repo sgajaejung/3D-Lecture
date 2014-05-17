906467f658cc300257a801ebface73fb9083b479
sgajaejung <sgajaejung@gmail.com>
2014-05-17 21:41:19 +0900

    update penguin journey

---
+++ PenguinJourney/PenguinJourney/DrawTriangle.cpp
@@ -0,0 +1,291 @@
+// 2014-04-04
+// http://joshbeam.com/articles/triangle_rasterization/
+
+#include "math/Math.h"
+#include "DrawTriangle.h"
+#include <math.h>
+
+
+namespace Rasterizer
+{
+	struct Edge
+	{
+		Color Color1, Color2;
+		Vector3 N1, N2;
+		int X1, Y1, X2, Y2;
+		Edge(const Color &color1, int x1, int y1, const Vector3 &n1, const Color &color2, int x2, int y2, const Vector3 &n2);
+	};
+
+	struct Span
+	{
+		Color Color1, Color2;
+		Vector3 N1, N2;
+		int X1, X2;
+		Span(const Color &color1, int x1, const Vector3 &n1, const Color &color2, int x2, const Vector3 &n2);
+	};
+
+
+	void DrawSpansBetweenEdges(HDC hdc, const Edge &e1, const Edge &e2);
+	void DrawSpan(HDC hdc, const Span &span, int y);
+
+
+	Vector3 g_LightDir = Vector3(0,-1,0);
+	void SetLight( const Vector3 &dir ) { g_LightDir = dir; }
+}
+
+
+using namespace Rasterizer;
+
+
+Edge::Edge(const Color &color1, int x1, int y1, const Vector3 &n1, const Color &color2, int x2, int y2, const Vector3 &n2)
+{
+	if(y1 < y2) {
+		Color1 = color1;
+		X1 = x1;
+		Y1 = y1;
+		N1 = n1;
+		Color2 = color2;
+		X2 = x2;
+		Y2 = y2;
+		N2 = n2;
+	} else {
+		Color1 = color2;
+		X1 = x2;
+		Y1 = y2;
+		N1 = n2;
+		Color2 = color1;
+		X2 = x1;
+		Y2 = y1;
+		N2 = n1;
+	}
+
+	//X1 = max(0, X1);
+	//X1 = min(X1, 800);
+	//X2 = max(0, X2);
+	//X2 = min(X2, 800);
+	//Y1 = max(0, Y1);
+	//Y1 = min(Y1, 600);
+	//Y2 = max(0, Y2);
+	//Y2 = min(Y2, 600);
+}
+
+
+Span::Span(const Color &color1, int x1, const Vector3 &n1, const Color &color2, int x2, const Vector3 &n2)
+{
+	if(x1 < x2) {
+		Color1 = color1;
+		X1 = x1;
+		N1 = n1;
+		Color2 = color2;
+		X2 = x2;
+		N2 = n2;
+	} else {
+		Color1 = color2;
+		X1 = x2;
+		N1 = n2;
+		Color2 = color1;
+		X2 = x1;
+		N2 = n1;
+	}
+
+	//X1 = max(0, X1);
+	//X1 = min(X1, 800);
+	//X2 = max(0, X2);
+	//X2 = min(X2, 800);
+}
+
+Color::Color(float r, float g, float b, float a) : R(r), G(g), B(b), A(a)
+{
+}
+
+Color Color::operator + (const Color &c) const
+{
+	return Color(min(R+c.R, 255), 
+		min(G+c.G, 255), 
+		min(B+c.B, 255), 
+		min(A+c.A, 255));
+}
+
+Color Color::operator - (const Color &c) const
+{
+	return Color(R-c.R, G-c.G, B-c.B, A-c.A);
+}
+
+Color Color::operator * (float f) const
+{
+	return Color(R*f, G*f, B*f, A*f);
+}
+
+
+/**
+ @brief 
+ @date 2014-04-04
+*/
+void Rasterizer::DrawTriangle(HDC hdc, 
+	const Color &color1, float x1, float y1, const Vector3 &norm1,
+	const Color &color2, float x2, float y2, const Vector3 &norm2,
+	const Color &color3, float x3, float y3, const Vector3 &norm3)
+{
+	// create edges for the triangle
+	Edge edges[3] = {
+		Edge(color1, (int)x1, (int)y1, norm1, color2, (int)x2, (int)y2, norm2),
+		Edge(color2, (int)x2, (int)y2, norm2, color3, (int)x3, (int)y3, norm3),
+		Edge(color3, (int)x3, (int)y3, norm3, color1, (int)x1, (int)y1, norm1)
+	};
+
+	int maxLength = 0;
+	int longEdge = 0;
+
+	// find edge with the greatest length in the y axis
+	for(int i = 0; i < 3; i++) {
+		int length = edges[i].Y2 - edges[i].Y1;
+		if(length > maxLength) {
+			maxLength = length;
+			longEdge = i;
+		}
+	}
+
+	int shortEdge1 = (longEdge + 1) % 3;
+	int shortEdge2 = (longEdge + 2) % 3;
+
+	// draw spans between edges; the long edge can be drawn
+	// with the shorter edges to draw the full triangle
+	DrawSpansBetweenEdges(hdc, edges[longEdge], edges[shortEdge1]);
+	DrawSpansBetweenEdges(hdc, edges[longEdge], edges[shortEdge2]);
+}
+
+
+void Rasterizer::DrawSpansBetweenEdges(HDC hdc, const Edge &e1, const Edge &e2)
+{
+	// calculate difference between the y coordinates
+	// of the first edge and return if 0
+	float e1ydiff = (float)(e1.Y2 - e1.Y1);
+	if(e1ydiff == 0.0f)
+		return;
+
+	// calculate difference between the y coordinates
+	// of the second edge and return if 0
+	float e2ydiff = (float)(e2.Y2 - e2.Y1);
+	if(e2ydiff == 0.0f)
+		return;
+
+	// calculate differences between the x coordinates
+	// and colors of the points of the edges
+	float e1xdiff = (float)(e1.X2 - e1.X1);
+	float e2xdiff = (float)(e2.X2 - e2.X1);
+	Color e1colordiff = (e1.Color2 - e1.Color1);
+	Color e2colordiff = (e2.Color2 - e2.Color1);
+
+	// calculate factors to use for interpolation
+	// with the edges and the step values to increase
+	// them by after drawing each span
+	float factor1 = (float)(e2.Y1 - e1.Y1) / e1ydiff;
+	float factorStep1 = 1.0f / e1ydiff;
+	float factor2 = 0.0f;
+	float factorStep2 = 1.0f / e2ydiff;
+
+	// loop through the lines between the edges and draw spans
+	for(int y = e2.Y1; y < e2.Y2; y++) {
+		// create and draw span
+		Span span(e1.Color1 + (e1colordiff * factor1),
+			e1.X1 + (int)(e1xdiff * factor1),
+			e1.N1,
+			e2.Color1 + (e2colordiff * factor2),
+			e2.X1 + (int)(e2xdiff * factor2),
+			e2.N1);
+		DrawSpan(hdc, span, y);
+
+		// increase factors
+		factor1 += factorStep1;
+		factor2 += factorStep2;
+	}
+}
+
+
+void Rasterizer::DrawSpan(HDC hdc, const Span &span, int y)
+{
+	int xdiff = span.X2 - span.X1;
+	if(xdiff == 0)
+		return;
+
+	Color colordiff = span.Color2 - span.Color1;
+
+	float factor = 0.0f;
+	float factorStep = 1.0f / (float)xdiff;
+
+	// draw each pixel in the span
+	for (int x = span.X1; x < span.X2; x++) {
+
+		Color c = span.Color1 + (colordiff * factor);
+		COLORREF color = RGB(c.R, c.G, c.B);
+
+		SetPixel(hdc, x, y, color);
+		factor += factorStep;
+	}
+}
+
+
+void Rasterizer::DrawLine(HDC hdc, const Color &color1, float x1, float y1,
+	const Color &color2, float x2, float y2)
+{
+	float xdiff = (x2 - x1);
+	float ydiff = (y2 - y1);
+
+	if(xdiff == 0.0f && ydiff == 0.0f) {
+		COLORREF color = RGB(color1.R, color1.G, color1.B);
+		SetPixel(hdc, (int)x1, (int)y1, color);
+		return;
+	}
+
+	if(fabs(xdiff) > fabs(ydiff)) {
+		float xmin, xmax;
+
+		// set xmin to the lower x value given
+		// and xmax to the higher value
+		if(x1 < x2) {
+			xmin = x1;
+			xmax = x2;
+		} else {
+			xmin = x2;
+			xmax = x1;
+		}
+
+		xmin = max(0, xmin);
+		xmax = min(xmax, 800);
+
+		// draw line in terms of y slope
+		float slope = ydiff / xdiff;
+		for(float x = xmin; x <= xmax; x += 1.0f) {
+			float y = y1 + ((x - x1) * slope);
+			Color color = color1 + ((color2 - color1) * ((x - x1) / xdiff));
+			COLORREF c = RGB(color.R, color.G, color.B);
+			SetPixel(hdc, (int)x, (int)y, c);
+		}
+
+	} else {
+		float ymin, ymax;
+
+		// set ymin to the lower y value given
+		// and ymax to the higher value
+		if(y1 < y2) {
+			ymin = y1;
+			ymax = y2;
+		} else {
+			ymin = y2;
+			ymax = y1;
+		}
+
+		ymin = max(0, ymin);
+		ymax = min(ymax, 600);
+
+		// draw line in terms of x slope
+		float slope = xdiff / ydiff;
+		for(float y = ymin; y <= ymax; y += 1.0f) {
+			float x = x1 + ((y - y1) * slope);
+			Color color = color1 + ((color2 - color1) * ((y - y1) / ydiff));
+			COLORREF c = RGB(color.R, color.G, color.B);
+			SetPixel(hdc, (int)x, (int)y, c);
+		}
+	}
+}
+
---
+++ PenguinJourney/PenguinJourney/PenguinJourney.cpp
@@ -0,0 +1,400 @@
+#include <windows.h>
+#include "math/Math.h"
+#include "DrawTriangle.h"
+#include <vector>
+#include <fstream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+const int WINSIZE_X = 800;		//초기 윈도우 가로 크기
+const int WINSIZE_Y = 600;	//초기 윈도우 세로 크기
+const int WINPOS_X = 0; //초기 윈도우 시작 위치 X
+const int WINPOS_Y = 0; //초기 윈도우 시작 위치 Y
+
+HWND g_hWnd;
+vector<Vector3> g_vertices;
+vector<Vector3> g_normals;
+vector<int> g_indices;
+Matrix44 g_matWorld;
+Matrix44 g_matLocal;
+Matrix44 g_matView;
+Matrix44 g_matProjection;
+Matrix44 g_matViewPort;
+Vector3 g_cameraPos(0,1000,-1000);
+Vector3 g_cameraLookat(0,0,0);
+
+
+// 콜백 프로시져 함수 프로토 타입
+LRESULT CALLBACK WndProc( HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam );
+void MainLoop(int elapse_time);
+void	Render(HWND hWnd);
+void	Paint(HWND hWnd, HDC hdc);
+void	ComputeNormals(const vector<Vector3> &vertices, const vector<int> &indices, vector<Vector3> &normals);
+bool	ReadModelFile( const string &fileName );
+bool Init();
+
+int APIENTRY WinMain(HINSTANCE hInstance, 
+	HINSTANCE hPrevInstance, 
+	LPSTR lpCmdLine, 
+	int nCmdShow)
+{
+	wchar_t className[32] = L"Sample";
+	wchar_t windowName[32] = L"Sample";
+
+	//윈도우 클레스 정보 생성
+	//내가 이러한 윈도를 만들겠다 라는 정보
+	WNDCLASS WndClass;
+	WndClass.cbClsExtra = 0;			//윈도우에서 사용하는 여분의 메모리설정( 그냥 0 이다  신경쓰지말자 )
+	WndClass.cbWndExtra = 0;			//윈도우에서 사용하는 여분의 메모리설정( 그냥 0 이다  신경쓰지말자 )
+	WndClass.hbrBackground = (HBRUSH)GetStockObject(GRAY_BRUSH);		//윈도우 배경색상
+	WndClass.hCursor = LoadCursor( NULL, IDC_ARROW );			//윈도우의 커서모양 결정
+	WndClass.hIcon = LoadIcon( NULL, IDI_APPLICATION );		//윈도우아이콘모양 결정
+	WndClass.hInstance = hInstance;				//프로그램인스턴스핸들 
+	WndClass.lpfnWndProc = (WNDPROC)WndProc;			//윈도우 프로시져 함수 포인터
+	WndClass.lpszMenuName = NULL;						//메뉴이름 없으면 NULL
+	WndClass.lpszClassName = className;				//지금 작성하고 있는 윈도우 클레스의 이름
+	WndClass.style	 = CS_HREDRAW | CS_VREDRAW;	//윈도우 그리기 방식 설정 ( 사이즈가 변경될때 화면갱신 CS_HREDRAW | CS_VREDRAW )
+
+	//위에서 작성한 윈도우 클레스정보 등록
+	RegisterClass( &WndClass );
+
+	//윈도우 생성
+	//생성된 윈도우 핸들을 전역변수 g_hWnd 가 받는다.
+	HWND hWnd = CreateWindow(
+		className,				//생성되는 윈도우의 클래스이름
+		windowName,				//윈도우 타이틀바에 출력되는 이름
+		WS_OVERLAPPEDWINDOW,	//윈도우 스타일 WS_OVERLAPPEDWINDOW
+		WINPOS_X,				//윈도우 시작 위치 X 
+		WINPOS_Y,				//윈도우 시작 위치 Y
+		WINSIZE_X,				//윈도우 가로 크기 ( 작업영역의 크기가 아님 )
+		WINSIZE_Y,				//윈도우 세로 크기 ( 작업영역의 크기가 아님 )
+		GetDesktopWindow(),		//부모 윈도우 핸들 ( 프로그램에서 최상위 윈도우면 NULL 또는 GetDesktopWindow() )
+		NULL,					//메뉴 ID ( 자신의 컨트롤 객체의 윈도우인경우 컨트롤 ID 가 된	
+		hInstance,				//이 윈도우가 물릴 프로그램 인스턴스 핸들
+		NULL					//추가 정보 NULL ( 신경끄자 )
+		);
+
+	g_hWnd = hWnd;
+
+	//윈도우를 정확한 작업영역 크기로 맞춘다
+	RECT rcClient = { 0, 0, WINSIZE_X, WINSIZE_Y };
+	AdjustWindowRect( &rcClient, WS_OVERLAPPEDWINDOW, FALSE );	//rcClient 크기를 작업 영영으로 할 윈도우 크기를 rcClient 에 대입되어 나온다.
+
+	//윈도우 크기와 윈도우 위치를 바꾸어준다.
+	SetWindowPos( hWnd, NULL, 0, 0, rcClient.right - rcClient.left, rcClient.bottom - rcClient.top, 
+		SWP_NOZORDER | SWP_NOMOVE );
+
+	Init();
+
+	ShowWindow( hWnd, nCmdShow );
+
+	//메시지 구조체
+	MSG msg;		
+	ZeroMemory( &msg, sizeof( MSG ) );
+
+	int oldT = GetTickCount();
+	while (msg.message != WM_QUIT)
+	{
+		//PeekMessage 는 메시지 큐에 메시지가 없어도 프로그램이 멈추기 않고 진행이 된다.
+		//이때 메시지큐에 메시지가 없으면 false 가 리턴되고 메시지가 있으면 true 가 리턴이된다.
+		if (PeekMessage(&msg, 0, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage( &msg ); //눌린 키보드 의 문자를 번역하여 WM_CHAR 메시지를 발생시킨다.
+			DispatchMessage( &msg );  //받아온 메시지 정보로 윈도우 프로시져 함수를 실행시킨다.
+		}
+		else
+		{
+			const int curT = GetTickCount();
+			const int elapseT = curT - oldT;
+			if (elapseT > 15)
+			{
+				oldT = curT;
+				MainLoop(elapseT);	
+			}
+		}
+	}
+
+	return 0;
+}
+
+
+//
+// 윈도우 프로시져 함수 ( 메시지 큐에서 받아온 메시지를 처리한다 )
+//
+LRESULT CALLBACK WndProc( HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam )
+{
+	PAINTSTRUCT ps;
+	HDC hdc;
+
+	switch (msg)
+	{
+	case WM_ERASEBKGND:
+		return 0;
+	case WM_PAINT:
+		hdc = BeginPaint(hWnd, &ps);
+		Paint(hWnd, hdc);
+		EndPaint(hWnd, &ps);
+		break;
+	case WM_KEYDOWN:
+		if (wParam == VK_ESCAPE)
+			::DestroyWindow(hWnd);
+		break;
+	case WM_DESTROY: //윈도우가 파괴된다면..
+		PostQuitMessage(0);	//프로그램 종료 요청 ( 메시지 루프를 빠져나가게 된다 )
+		break;
+	}
+	return DefWindowProc( hWnd, msg, wParam, lParam );
+}
+
+
+/**
+ @brief 
+ */
+void	MainLoop(int elapse_time)
+{
+	// Render
+	Render(g_hWnd);
+	::InvalidateRect(g_hWnd, NULL, TRUE);
+}
+
+
+/**
+ @brief 
+ */
+void	Render(HWND hWnd)
+{
+	HDC hdc = GetDC(hWnd);
+	Paint(hWnd, hdc);
+	::ReleaseDC(hWnd, hdc);
+}
+
+
+bool Init()
+{
+	ReadModelFile("vase.dat");
+
+	Vector3 dir = g_cameraLookat - g_cameraPos;
+	dir.Normalize();
+	g_matView.SetView(g_cameraPos, dir, Vector3(0,1,0));
+	g_matProjection.SetProjection( MATH_PI / 4.f, 1.0f, 1.0f, 100.0f );
+
+	RECT cr;
+	::GetClientRect(g_hWnd, &cr);
+	const float width = 800;//(float)(cr.right-cr.left);
+	const float height = 600;//(float)(cr.bottom - cr.top);
+	g_matViewPort._11 = width/2;
+	g_matViewPort._22 = -height/2;
+	g_matViewPort._33 = 0;
+	g_matViewPort._41 = width/2;
+	g_matViewPort._42 = height/2;
+	g_matViewPort._43 = 0;
+
+	return true;
+}
+
+
+/**
+ @brief 
+ @date 2014-04-07
+*/
+void ComputeNormals(const vector<Vector3> &vertices, const vector<int> &indices, vector<Vector3> &normals)
+{
+	g_normals.resize(vertices.size());
+
+	for (unsigned int i=0; i < indices.size(); i+=3)
+	{
+		Vector3 p1 = vertices[ indices[ i]];
+		Vector3 p2 = vertices[ indices[ i+1]];
+		Vector3 p3 = vertices[ indices[ i+2]];
+
+		Vector3 v1 = p2 - p1;
+		Vector3 v2 = p3 - p1;
+		v1.Normalize();
+		v2.Normalize();
+		Vector3 n = v1.CrossProduct(v2);
+		n.Normalize();
+
+		if (g_normals[ indices[ i]].IsEmpty())
+		{
+			g_normals[ indices[ i]] = n;
+		}
+		else
+		{
+			g_normals[ indices[ i]] += n;
+			g_normals[ indices[ i]] /= 2.f;
+		}
+
+		if (g_normals[ indices[ i+1]].IsEmpty())
+		{
+			g_normals[ indices[ i+1]] = n;
+		}
+		else
+		{
+			g_normals[ indices[ i+1]] += n;
+			g_normals[ indices[ i+1]] /= 2.f;
+		}
+
+		if (g_normals[ indices[ i+2]].IsEmpty())
+		{
+			g_normals[ indices[ i+2]] = n;
+		}
+		else
+		{
+			g_normals[ indices[ i+2]] += n;
+			g_normals[ indices[ i+2]] /= 2.f;
+		}
+	}
+}
+
+
+bool ReadModelFile( const string &fileName )
+{
+	using namespace std;
+	ifstream fin(fileName.c_str());
+	if (!fin.is_open())
+		return false;
+
+	string vtx, vtx_eq;
+	int numVertices;
+	fin >> vtx >> vtx_eq >> numVertices;
+
+	if (numVertices <= 0)
+		return  false;
+
+	g_vertices.resize(numVertices);
+
+	float num1, num2, num3;
+	for (int i = 0; i < numVertices; i++)
+	{
+		fin >> num1 >> num2 >> num3;
+		g_vertices[i] = Vector3(num1, num2, num3);
+	}
+
+	string idx, idx_eq;
+	int numIndices;
+	fin >> idx >> idx_eq >> numIndices;
+
+	if (numIndices <= 0)
+		return false;
+
+	g_indices.resize(numIndices*3);
+
+	int num4, num5, num6;
+	for (int i = 0; i < numIndices*3; i+=3)
+	{
+		fin >> num4 >> num5 >> num6;
+		g_indices[ i] = num4;
+		g_indices[ i+1] = num5;
+		g_indices[ i+2] = num6;	
+	}
+
+	ComputeNormals(g_vertices, g_indices, g_normals);
+
+	return true;
+}
+
+
+void RenderVertices(HDC hdc, const vector<Vector3> &vertices, const Matrix44 &tm)
+{
+	for (unsigned int i=0; i < vertices.size(); ++i)
+	{
+		Vector3 p = vertices[ i] * tm;
+
+		if (0 == i)
+			MoveToEx(hdc, (int)p.x, (int)p.y, NULL);
+		else
+			LineTo(hdc, (int)p.x, (int)p.y);
+	}
+}
+
+
+void RenderIndices(HDC hdc, const vector<Vector3> &vertices, const vector<int> &indices, 
+				   const vector<Vector3> &normals, const Matrix44 &tm, const Matrix44 &vpv)
+{
+	Vector3 camDir = g_cameraLookat - g_cameraPos;
+	camDir.Normalize();
+
+	for (unsigned int i=0; i < indices.size(); i+=3)
+	{
+		Vector3 p1 = vertices[ indices[ i]];
+		Vector3 p2 = vertices[ indices[ i+1]];
+		Vector3 p3 = vertices[ indices[ i+2]];
+
+		p1 = p1 * tm;
+		p2 = p2 * tm;
+		p3 = p3 * tm;
+
+		// culling
+		Vector3 n1 = normals[ indices[i]] * tm;
+		Vector3 n2 = normals[ indices[i+1]] * tm;
+		Vector3 n3 = normals[ indices[i+2]] * tm;
+		const float dot1 = n1.DotProduct(camDir);
+		const float dot2 = n2.DotProduct(camDir);
+		const float dot3 = n3.DotProduct(camDir);
+		if ((dot1 > 0) && (dot2 > 0) && (dot3 > 0))
+			continue;
+
+		p1 = p1 * vpv;
+		p2 = p2 * vpv;
+		p3 = p3 * vpv;
+
+		Rasterizer::Color c0(0,0,255,1);
+		Rasterizer::Color c1(255,255,255,1);
+		Vector3 lightDir(0,-1,0);
+		Vector3 H = -(camDir + lightDir);
+		H.Normalize();
+		Rasterizer::Color ambient(0,20,0,1);
+
+		Rasterizer::Color color1, color2, color3;
+		{
+			Rasterizer::Color diffuse = c0 * max(0, n1.DotProduct(-lightDir));
+			Rasterizer::Color specular = c1*pow(n1.DotProduct(H), 16);
+			color1 = ambient + diffuse + specular;
+		}
+		{
+			Rasterizer::Color diffuse = c0 * max(0, n2.DotProduct(-lightDir));
+			Rasterizer::Color specular = c1*pow(n2.DotProduct(H), 16);
+			color2 = ambient + diffuse + specular;
+		}
+		{
+			Rasterizer::Color diffuse = c0 * max(0, n3.DotProduct(-lightDir));
+			Rasterizer::Color specular = c1*pow(n3.DotProduct(H), 16);
+			color3 = ambient + diffuse + specular;
+		}
+
+//		Rasterizer::DrawLine(hdc, color, p1.x, p1.y, color, p2.x, p2.y);
+//		Rasterizer::DrawLine(hdc, color, p1.x, p1.y, color, p3.x, p3.y);
+//		Rasterizer::DrawLine(hdc, color, p3.x, p3.y, color, p2.x, p2.y);
+		Rasterizer::DrawTriangle(hdc, 
+			color1, p1.x, p1.y, n1,
+			color2, p2.x, p2.y, n2,
+			color3, p3.x, p3.y, n3);
+	}
+}
+
+
+/**
+ @brief 
+ */
+void Paint(HWND hWnd, HDC hdc)
+{
+	RECT rc;
+	GetClientRect(hWnd, &rc);
+	HDC hdcMem = CreateCompatibleDC(hdc);
+	HBITMAP hbmMem = CreateCompatibleBitmap(hdc, rc.right-rc.left, rc.bottom-rc.top);
+	HBITMAP hbmOld = (HBITMAP)SelectObject(hdcMem, hbmMem);
+	HBRUSH hbrBkGnd = CreateSolidBrush(GetSysColor(COLOR_WINDOW));
+	FillRect(hdcMem, &rc, hbrBkGnd);
+	DeleteObject(hbrBkGnd);
+
+	Matrix44 vpv = g_matView * g_matProjection * g_matViewPort;
+	RenderIndices(hdcMem, g_vertices, g_indices, g_normals, g_matLocal * g_matWorld, vpv);
+
+	BitBlt(hdc, rc.left, rc.top, rc.right-rc.left, rc.bottom-rc.top, hdcMem, 0, 0, SRCCOPY);
+	SelectObject(hdcMem, hbmOld);
+	DeleteObject(hbmMem);
+	DeleteDC(hdcMem);
+}
+
