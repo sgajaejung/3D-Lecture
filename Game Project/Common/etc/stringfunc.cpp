d9507f8b0944a0a9ce311d8f8cfd5343d6a2c713
sgajaejung <sgajaejung@gmail.com>
2014-06-07 02:37:49 +0900

    update stringfunc.h add format, wstr2str

--- Game Project/Common/etc/stringfunc.cpp
+++ Game Project/Common/etc/stringfunc.cpp
@@ -35,3 +35,62 @@ string& common::trim(string &str)
 	replaceAll(str, "$", "");
 	return str;
 }
+
+
+//------------------------------------------------------------------------
+// 유니코드를 멀티바이트 문자로 변환
+//------------------------------------------------------------------------
+std::string common::wstr2str(const std::wstring &wstr)
+{
+	const int slength = (int)wstr.length() + 1;
+	const int len = ::WideCharToMultiByte(CP_ACP, 0, wstr.c_str(), slength, 0, 0, NULL, FALSE);
+	char* buf = new char[len];
+	::WideCharToMultiByte(CP_ACP, 0, wstr.c_str(), slength, buf, len, NULL, FALSE);
+	std::string r(buf);
+	delete[] buf;
+	return r;
+}
+
+
+//------------------------------------------------------------------------
+// 멀티바이트 문자를 유니코드로 변환
+//------------------------------------------------------------------------
+std::wstring common::str2wstr(const std::string &str)
+{
+	int len;
+	int slength = (int)str.length() + 1;
+	len = ::MultiByteToWideChar(CP_ACP, 0, str.c_str(), slength, 0, 0);
+	wchar_t* buf = new wchar_t[len];
+	::MultiByteToWideChar(CP_ACP, 0, str.c_str(), slength, buf, len);
+	std::wstring r(buf);
+	delete[] buf;
+	return r;
+}
+
+
+//------------------------------------------------------------------------
+// 스트링포맷
+//------------------------------------------------------------------------
+std::string common::format(const char* fmt, ...)
+{
+	char textString[ 256] = {'\0'};
+	va_list args;
+	va_start ( args, fmt );
+	vsnprintf_s( textString, sizeof(textString), _TRUNCATE, fmt, args );
+	va_end ( args );
+	return textString;
+}
+
+
+//------------------------------------------------------------------------
+// 스트링포맷 wstring 용
+//------------------------------------------------------------------------
+std::wstring common::formatw(const char* fmt, ...)
+{
+	char textString[ 256] = {'\0'};
+	va_list args;
+	va_start ( args, fmt );
+	vsnprintf_s( textString, sizeof(textString), _TRUNCATE, fmt, args );
+	va_end ( args );
+	return str2wstr(textString);
+}
