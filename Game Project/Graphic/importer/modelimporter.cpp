6eae53714e8bebf0fbbf9bd689576fe39596996c
sgajaejung <sgajaejung@gmail.com>
2014-06-07 14:11:21 +0900

    update model importer

--- Game Project/Graphic/importer/modelimporter.cpp
+++ Game Project/Graphic/importer/modelimporter.cpp
@@ -16,10 +16,13 @@
 
 namespace graphic { namespace importer {
 
-	bool ReadRawMeshFileV1( const string &fileName, OUT sRawMesh &raw );
-	bool ReadRawMeshFileV2( const string &fileName, OUT sRawMesh &raw );
-	bool ReadRawMeshFileV3( const string &fileName, OUT sRawMesh &raw, OUT sRawAni &rawAni );
+	bool ReadRawMeshFileV1( const string &fileName, OUT sRawMesh &rawMesh );
+	bool ReadRawMeshFileV2( const string &fileName, OUT sRawMesh &rawMesh );
+	bool ReadRawMeshFileV3( const string &fileName, OUT sRawMesh &rawMesh, OUT sRawAni &rawAni );
 
+	bool ReadVertexIndexNormal( std::ifstream &fin, OUT sRawMesh &rawMesh );
+	bool ReadTextureCoordinate( std::ifstream &fin, const string &fileName, OUT sRawMesh &rawMesh );
+	bool ReadAnimation(std::ifstream &fin, OUT sRawMesh &rawMesh, OUT sRawAni &rawAni );
 }}
 
 using namespace graphic;
@@ -64,77 +67,9 @@ bool importer::ReadRawMeshFileV1( const string &fileName, OUT sRawMesh &rawMesh 
 		return false;
 
 	string exporterVersion;
-	fin >>exporterVersion;
+	fin >> exporterVersion;
 
-	if (exporterVersion != "EXPORTER_V1")		
-		return false;
-
-	string vtx, eq;
-	int vtxSize;
-	fin >> vtx >> eq >> vtxSize;
-
-	if (vtxSize <= 0)
-		return  false;
-
-	rawMesh.vertices.reserve(vtxSize + vtxSize/2);
-
-	float num1, num2, num3;
-	for (int i = 0; i < vtxSize; i++)
-	{
-		fin >> num1 >> num2 >> num3;
-		rawMesh.vertices.push_back( Vector3(num1, num2, num3) );
-	}
-
-
-	// 인덱스 버퍼 초기화.
-	string idx;
-	int faceSize;
-	fin >> idx >> eq >> faceSize;
-
-	rawMesh.indices.reserve(faceSize);
-
-	if (faceSize > 0)
-	{
-		int num1, num2, num3;
-		for (int i = 0; i < faceSize*3; i+=3)
-		{
-			fin >> num1 >> num2 >> num3;
-			rawMesh.indices.push_back(num1);
-			rawMesh.indices.push_back(num2);
-			rawMesh.indices.push_back(num3);
-		}
-	}
-
-	string norm;
-	int numNormal;
-	fin >> norm >> eq >> numNormal;
-	rawMesh.normals.resize(vtxSize);
-
-	if (numNormal > 0)
-	{
-		float num1, num2, num3;
-		vector<int> vertCount(vtxSize, 0);
-		for (int i = 0; i < numNormal; i++)
-		{
-			fin >> num1 >> num2 >> num3;
-			Vector3 n(num1, num2, num3);
-
-			// 법선벡터의 평균을 구해서 할당한다.
-			for (int k=0; k < 3; ++k)
-			{
-				const int vtxIdx = rawMesh.indices[ i*3 + k];
-				rawMesh.normals[ vtxIdx] += n;
-				++vertCount[ vtxIdx];
-			}
-		}
-
-		for (int i=0; i < vtxSize; ++i)
-		{
-			rawMesh.normals[ i] /= (float)vertCount[ i];
-			rawMesh.normals[ i].Normalize();
-		}
-	}
-
+	ReadVertexIndexNormal(fin, rawMesh);
 	return true;
 }
 
@@ -148,187 +83,12 @@ bool importer::ReadRawMeshFileV2( const string &fileName, OUT sRawMesh &rawMesh 
 		return false;
 
 	string exporterVersion;
-	fin >>exporterVersion;
+	fin >> exporterVersion;
 
-	if (exporterVersion != "EXPORTER_V2")
-		return false;
-
-	string vtx, eq;
-	int vtxSize;
-	fin >> vtx >> eq >> vtxSize;
-
-	if (vtxSize <= 0)
-		return  false;
-
-	rawMesh.vertices.reserve(vtxSize + vtxSize/2);
-
-	float num1, num2, num3;
-	for (int i = 0; i < vtxSize; i++)
-	{
-		fin >> num1 >> num2 >> num3;
-		rawMesh.vertices.push_back( Vector3(num1, num2, num3) );
-	}
-
-
-	// 인덱스 버퍼 초기화.
-	string idx;
-	int faceSize;
-	fin >> idx >> eq >> faceSize;
-
-	rawMesh.indices.reserve(faceSize);
-
-	if (faceSize > 0)
-	{
-		int num1, num2, num3;
-		for (int i = 0; i < faceSize*3; i+=3)
-		{
-			fin >> num1 >> num2 >> num3;
-			rawMesh.indices.push_back(num1);
-			rawMesh.indices.push_back(num2);
-			rawMesh.indices.push_back(num3);
-		}
-	}
-
-	string norm;
-	int numNormal;
-	fin >> norm >> eq >> numNormal;
-	rawMesh.normals.resize(vtxSize);
-
-	if (numNormal > 0)
-	{
-		float num1, num2, num3;
-		vector<int> vertCount(vtxSize, 0);
-		for (int i = 0; i < numNormal; i++)
-		{
-			fin >> num1 >> num2 >> num3;
-			Vector3 n(num1, num2, num3);
-
-			// 법선벡터의 평균을 구해서 할당한다.
-			for (int k=0; k < 3; ++k)
-			{
-				const int vtxIdx = rawMesh.indices[ i*3 + k];
-				rawMesh.normals[ vtxIdx] += n;
-				++vertCount[ vtxIdx];
-			}
-		}
-
-		for (int i=0; i < vtxSize; ++i)
-		{
-			rawMesh.normals[ i] /= (float)vertCount[ i];
-			rawMesh.normals[ i].Normalize();
-		}
-	}
-
-
-
-	string tex;
-	int numTex;
-	fin >> tex >> eq >> numTex;
-
-	rawMesh.tex.resize(vtxSize);
-
-	if (numTex > 0)
-	{
-		float fnum1, fnum2;
-		vector<Vector3> texVertices(numTex);
-		for (int i = 0; i < numTex; i++)
-		{
-			fin >> fnum1 >> fnum2;
-			texVertices[ i] = Vector3(fnum1, fnum2, 0);
-		}
-
-		string strTexFace;
-		int numTexFace;
-		fin >> strTexFace >> eq >> numTexFace;
-
-		vector<int> texFaces;
-		texFaces.reserve(numTexFace*3);
-		if (numTexFace > 0)
-		{
-			int num1, num2, num3;
-			for (int i=0; i < numTexFace; ++i)
-			{
-				fin >> num1 >> num2 >> num3;
-				texFaces.push_back( num1 );
-				texFaces.push_back( num2 );
-				texFaces.push_back( num3 );
-			}
-		}
-
-		map<int, vector<int> > vtxIdxMap; // vertex index, vertex index array
-		for (int i=0; i < vtxSize; ++i)
-		{
-			vector<int> varray;
-			varray.reserve(4);
-			varray.push_back(i);
-			vtxIdxMap[ i] = varray;
-		}
-
-		// 텍스쳐 좌표를 버텍스 버퍼에 저장한다. 
-		// 버텍스 버퍼의 uv 값이 초기화 되지 않았다면, 초기화 한다.
-		// 버텍스에 하나 이상의 uv값이 존재한다면, 버텍스를 추가하고, 인덱스버퍼를 수정한다.
-		for (int i=0; i < (int)texFaces.size(); ++i)
-		{
-			const Vector3 tex = texVertices[ texFaces[ i]];
-			const int vtxIdx = rawMesh.indices[ i];
-
-			bool isFind = false;
-			for (int k=0; k < (int)vtxIdxMap[ vtxIdx].size(); ++k)
-			{
-				const int subVtxIdx = vtxIdxMap[ vtxIdx][ k];
-
-				// 텍스쳐 좌표가 버텍스 버퍼에 저장되어 있다면, index buffer 값을 해당 vertex index 로
-				// 설정 한다.
-				if ((-100 == rawMesh.tex[ subVtxIdx].x) &&
-					(-100 == rawMesh.tex[ subVtxIdx].y))
-				{
-					rawMesh.tex[ subVtxIdx].x = tex.x;
-					rawMesh.tex[ subVtxIdx].y = tex.y;
-					isFind = true;
-					break;
-				}
-				else if ((tex.x == rawMesh.tex[ subVtxIdx].x) && 
-						    (tex.y == rawMesh.tex[ subVtxIdx].y))
-				{
-					rawMesh.indices[ i] = subVtxIdx;
-					isFind = true;
-					break;
-				}
-			}
-
-			// 버텍스 버퍼에 없는 uv 좌표라면, 새 버텍스를 버텍스버퍼에 추가한다.
-			// 인덱스 버퍼에도 새로 추가된 버텍스 인덱스값을 넣는다.
-			if (!isFind)
-			{
-				Vector3 p = rawMesh.vertices[ vtxIdx];
-				Vector3 n = rawMesh.normals[ vtxIdx];
-				Vector3 t = rawMesh.tex[ vtxIdx];
-
-				t.x = tex.x;
-				t.y = tex.y;
-
-				rawMesh.vertices.push_back(p);
-				rawMesh.normals.push_back(n);
-				rawMesh.tex.push_back(t);
-
-				const int newVtxIdx = rawMesh.vertices.size()-1;
-				vtxIdxMap[ vtxIdx].push_back( newVtxIdx );
-				rawMesh.indices[ i] = newVtxIdx;
-			}
-		}
-	}
-
-	// 텍스쳐 파일이름 로딩.
-	string textureTok, texFilePath;
-	fin >> textureTok >> eq;
-	std::getline(fin, texFilePath);
-	string  textureFileName = common::GetFilePathExceptFileName(fileName) + "\\" + 
-		common::trim(texFilePath);
-	rawMesh.texturePath = textureFileName;
-
+	ReadVertexIndexNormal(fin, rawMesh);
+	ReadTextureCoordinate(fin, fileName, rawMesh);
 	return true;
 }
-
 
 
 // load exporter version 3
@@ -343,9 +103,17 @@ bool importer::ReadRawMeshFileV3( const string &fileName,
 	string exporterVersion;
 	fin >>exporterVersion;
 
-	if (exporterVersion != "EXPORTER_V3")
-		return false;
+	ReadVertexIndexNormal(fin, rawMesh);
+	ReadTextureCoordinate(fin, fileName, rawMesh);
+	ReadAnimation(fin, rawMesh, rawAni);
+	return true;
+}
 
+
+// Read Vertex, Index, Normal Buffer
+// Normal 은 face 갯수만큼 존재해야 한다.
+bool importer::ReadVertexIndexNormal( std::ifstream &fin, OUT sRawMesh &rawMesh )
+{
 	string vtx, eq;
 	int vtxSize;
 	fin >> vtx >> eq >> vtxSize;
@@ -355,9 +123,9 @@ bool importer::ReadRawMeshFileV3( const string &fileName,
 
 	rawMesh.vertices.reserve(vtxSize + vtxSize/2);
 
+	float num1, num2, num3;
 	for (int i = 0; i < vtxSize; i++)
 	{
-		float num1, num2, num3;
 		fin >> num1 >> num2 >> num3;
 		rawMesh.vertices.push_back( Vector3(num1, num2, num3) );
 	}
@@ -385,7 +153,6 @@ bool importer::ReadRawMeshFileV3( const string &fileName,
 	string norm;
 	int numNormal;
 	fin >> norm >> eq >> numNormal;
-	
 	rawMesh.normals.resize(vtxSize);
 
 	if (numNormal > 0)
@@ -413,9 +180,16 @@ bool importer::ReadRawMeshFileV3( const string &fileName,
 		}
 	}
 
+	return true;
+}
 
 
-	string tex;
+// 텍스쳐 좌표를 읽어 온다.
+bool importer::ReadTextureCoordinate( std::ifstream &fin, const string &fileName, OUT sRawMesh &rawMesh )
+{
+	const int vtxSize = rawMesh.vertices.size();
+
+	string tex, eq;
 	int numTex;
 	fin >> tex >> eq >> numTex;
 
@@ -511,7 +285,6 @@ bool importer::ReadRawMeshFileV3( const string &fileName,
 			}
 		}
 	}
-	
 
 	// 텍스쳐 파일이름 로딩.
 	string textureTok, texFilePath;
@@ -521,15 +294,24 @@ bool importer::ReadRawMeshFileV3( const string &fileName,
 		common::trim(texFilePath);
 	rawMesh.texturePath = textureFileName;
 
-	
+	return true;
+}
+
+
+// 애니메이션 정보를 읽는다.
+bool importer::ReadAnimation(std::ifstream &fin, OUT sRawMesh &rawMesh, OUT sRawAni &rawAni )
+{
 	// 애니메이션 로딩.
 	float startT = 0;
 	float endT = 0;
-
-	string aniRange, strST, strET;
+	string aniRange, strST, strET, eq;
 	fin >> aniRange >> eq >> strST >> strET;
 	startT = (float)atof(strST.c_str());
 	endT = (float)atof(strET.c_str());
+
+	rawAni.start = startT;
+	rawAni.end = endT;
+
 
 	{ // 이동 애니메이션 로딩
 		string keypos;
@@ -570,9 +352,25 @@ bool importer::ReadRawMeshFileV3( const string &fileName,
 		}
 	}
 
-	rawAni.start = startT;
-	rawAni.end = endT;
+
+	{ // 스케일 애니메이션 로딩.
+		string keyscale;
+		int scaleSize;
+		fin >> keyscale >> eq >> scaleSize;
+
+		rawAni.scale.resize(scaleSize);
+
+		for (int i=0; i < scaleSize; ++i)
+		{
+			string frameScale, strT; // FRAME_SCALE
+			float x, y, z;
+			fin >> frameScale >> strT >> x >> y >> z;
+			const float t = (float)atof(strT.c_str());
+
+			rawAni.scale[ i].t = t;
+			rawAni.scale[ i].s = Vector3(x, y, z);
+		}
+	}
 
 	return true;
 }
-
