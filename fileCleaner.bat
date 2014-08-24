3d15d0311ccacad96b75213e66bcf3b8de0f50ec
sgajaejung <sgajaejung@gmail.com>
2014-08-25 01:55:35 +0900

    update fileCleaner

--- fileCleaner.bat
+++ fileCleaner.bat
@@ -1,2 +1,2 @@
-del /f /q /s /a rhsa *.bak *.ncb *.obj *.pdb *.idb *.ilk *.pch *.suo *.aps *.opt *.sbr *.bsc *.user
+del /f /q /s /a rhsa *.bak *.ncb *.obj *.pdb *.idb *.ilk *.pch *.suo *.aps *.opt *.sbr *.bsc *.user *.ipch *.tlog
 pause
